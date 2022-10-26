9c77fa13a00b327efcaeccdca77b21f30b915f5a
TonyNogueron <antonio.nogueron03@gmail.com>
2022-10-26 00:02:41 -0500

    Adds styles to table
    Co-authored-by: Kevin Arturo *-* <Kevin-ALR@users.noreply.github.com>

--- order-system/src/App.js
+++ order-system/src/App.js
@@ -1,11 +1,14 @@
 import orders from "./utils/testOrder.json";
 import Order from "./components/order-component/orderComponent";
+import styles from "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Orders</h1>
+      <div className="orderContainer">
       <Order props={orders} />
+      </div>
     </div>
   );
 }
