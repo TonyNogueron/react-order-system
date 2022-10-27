74a8dbe38a11b07fc3265801301562b5a08f0739
TonyNogueron <antonio.nogueron03@gmail.com>
2022-10-27 13:31:30 -0500

    adds home page

--- order-system-client/src/App.js
+++ order-system-client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ViewOrderPage from "./pages/view-order-page/viewOrderPage";
 import Formulario from "./pages/Formulario/Formulario";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home-page/Home";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
+      <Route exact path="/" element={<HomePage />} />
         <Route exact path="/viewOrder" element={<ViewOrderPage />} />
         <Route exact path="/form" element={<Formulario/>} />
       </Routes>
