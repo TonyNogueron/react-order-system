f198d6ccee41f5ad4906b3d9d1bdf9c62fae43da
TonyNogueron <antonio.nogueron03@gmail.com>
2022-10-27 12:18:30 -0500

    attempt of inputComponent

--- order-system-client/src/App.js
+++ order-system-client/src/App.js
@@ -1,15 +1,12 @@
 //import orders from "./utils/testOrder.json";
 import React from "react";
-import Order from "./components/order-component/orderComponent";
 import styles from "./App.css";
 import ViewOrderPage from "./pages/view-order-page/viewOrderPage";
-import Folio from "./components/folio-component/Folio";
 
 function App() {
   return (
     <div className="App">
       <ViewOrderPage />
-      <Folio />
     </div>
   );
 }
--- order-system-client/src/components/input-component/inputComponent.js
+++ order-system-client/src/components/input-component/inputComponent.js
@@ -1,11 +1,36 @@
 import React from "react";
 
 class Input extends React.Component {
-    constructor() {
-      super();
-      this.state = {color: "red"};
+  constructor(props) {
+    super(props);
+    this.state = { value: props.value };
+    this.handleChange = this.handleChange.bind(this);
     }
-    render() {
-      return <h2>I am a Car!</h2>;
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
     }
+
+  render() {
+    return (
+      <div className="Folio">
+        <h1>{this.props.title}</h1>
+        <div className="wrapper">
+          <input
+            type="text"
+            className="num"
+            value={this.state.value}
+            placeholder="Ingrese el num. de folio."
+            onChange={this.handleChange}
+          />
+        </div>
+      </div>
+    );
   }
+
+  getValue(){
+    return this.state.value;
+  }
+}
+
+export default Input;
