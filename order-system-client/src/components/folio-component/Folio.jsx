f142a4ecbff31b0c59b176c0573c443af050c79d
TonyNogueron <antonio.nogueron03@gmail.com>
2022-10-27 12:16:42 -0500

    attempt of input component

---
+++ order-system-client/src/components/folio-component/Folio.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Folio.css";
+import Input from "../input-component/inputComponent";
+
+
+
+function Folio() {
+    return (
+        <div>
+            <Input title="Hora" value=""/>
+       </div>
+    );
+}
+
+export default Folio;
