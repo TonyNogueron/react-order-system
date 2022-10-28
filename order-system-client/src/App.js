6164505de89de33947761229b5029b10f0753cbb
TonyNogueron <antonio.nogueron03@gmail.com>
2022-10-28 01:14:18 -0500

    adds edit order page

--- order-system-client/src/App.js
+++ order-system-client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ViewOrderPage from "./pages/view-order-page/viewOrderPage";
 import Formulario from "./pages/Formulario/Formulario";
 import HomePage from "./pages/home-page/Home";
-import EditOrder from "./pages/edit-order/editOrder";
+import EditPage from "./pages/edit-order/editPage";
 
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
@@ -13,7 +13,7 @@ function App() {
       <Route exact path="/" element={<HomePage />} />
         <Route exact path="/viewOrder" element={<ViewOrderPage />} />
         <Route exact path="/form" element={<Formulario/>} />
-        <Route exact path="/edit-order" element={<EditOrder/>} />
+        <Route exact path="/edit-order" element={<EditPage/>} />
       </Routes>
     </Router>
   );
